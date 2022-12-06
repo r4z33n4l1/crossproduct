2434511d8f5f69d453b05b71cb0d35c2019b9ffe
Razeen <razeen_ali@yahoo.com>
2022-12-06 09:38:21 -0500

    Revert "Merge pull request #11 from r4z33n4l1/case_fix"
    This reverts commit ed43a1a68ad46b5d2cea0b9c56f3cd716508f298, reversing
    changes made to 1c66e58fbf1a9005e2220c7662f26070816f8609.

--- code/boggle/BoggleGame.java
+++ code/boggle/BoggleGame.java
@@ -127,7 +127,7 @@ public class BoggleGame {
      */
     public String addWord(String s) {
         // TODO: Check if this word is already in the dictionary
-        if (allWords.containsKey(s.toLowerCase())) {
+        if (allWords.containsKey(s.toUpperCase())) {
             return this.gameMode.addWord(s);
         }
         return "Invalid Word";
