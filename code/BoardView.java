c30c8ae9f4d6059ff71a04c43797b2b420a97fa2
unknown <nipun.rustagi1@gmail.com>
2022-12-06 09:04:26 -0500

    Final GUI Setup Completed for all user based game options

--- code/BoardView.java
+++ code/BoardView.java
@@ -11,25 +11,60 @@ import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class BoardView {
     BoggleGame boggleGame;
     Stage stage;
-    public BoardView(BoggleGame boggleGame, Stage stage){
+
+    HomeView homeView;
+
+    private Button endButton;
+
+    private Button submitButton;
+
+    private Button backButton;
+
+    Label isValidWord;
+
+    TextField wordEntered;
+    public BoardView(HomeView view, BoggleGame boggleGame){
         this.boggleGame = boggleGame;
-        this.stage = stage;
+        this.homeView = view;
+        this.stage = view.stage;
+
         initUI();
     }
     private void initUI() {
+//        ArrayList<String> a = (homeView.color_mapping.get((homeView.board_colors.getValue())));
+        // convert a to array of strings
+
+        Object a = homeView.color_mapping.get((homeView.board_colors.getValue()));
+        List<String> b = Arrays.asList(a.toString().split(","));
+        // remove the brackets
+        b.set(0, b.get(0).substring(1));
+        b.set(b.size()-1, b.get(b.size()-1).substring(0, b.get(b.size()-1).length()-1));
+
+//        b.set(b.size()-2, b.get(b.size()-2).substring(0, b.get(b.size()-2).length()-1));
+        String background_color = b.get(0);
+        String text_color = b.get(1);
+        String button_color = b.get(2);
+//        String button_color = b.get(2);
+//        System.out.println(button_color);
+
         SplitPane splitPane0 = new SplitPane();
         splitPane0.setPrefHeight(439.0);
         splitPane0.setPrefWidth(625.0);
         splitPane0.setDividerPositions(0.6);
+        //make the divider transparent
         AnchorPane anchorPane0 = new AnchorPane();
         anchorPane0.setPrefHeight(200.0);
         anchorPane0.setPrefWidth(200.0);
+        anchorPane0.setStyle("-fx-background-color: " + background_color + ";");
         StackPane stackPane0 = new StackPane();
         stackPane0.setPrefHeight(429.0);
         stackPane0.setPrefWidth(361.0);
@@ -37,6 +72,7 @@ public class BoardView {
         stackPane0.setLayoutY(4.0);
         stackPane0.setMouseTransparent(true);
         Label boggleboardDisplay = new Label();
+        boggleboardDisplay.setStyle("-fx-background-color: " + background_color + "; -fx-text-fill: " + text_color + ";");
         boggleboardDisplay.setFont(Font.font("System", FontWeight.BOLD, 35.0));
         stackPane0.getChildren().add(boggleboardDisplay);
         anchorPane0.getChildren().add(stackPane0);
@@ -44,42 +80,84 @@ public class BoardView {
         AnchorPane anchorPane1 = new AnchorPane();
         anchorPane1.setPrefHeight(437.0);
         anchorPane1.setPrefWidth(275.0);
+        anchorPane1.setStyle("-fx-background-color: " + background_color + ";");
         Label wordLabel = new Label();
-        wordLabel.setLayoutX(18.0);
-        wordLabel.setLayoutY(67.0);
+        wordLabel.setLayoutX(17.0);
+        wordLabel.setLayoutY(14.0);
+        wordLabel.setStyle("-fx-background-color: " + background_color + "; -fx-text-fill: " + text_color + ";");
         wordLabel.setText("Enter word found below:");
         wordLabel.setFont(Font.font("System", FontWeight.BOLD, 18.0));
         anchorPane1.getChildren().add(wordLabel);
         TextField wordEntered = new TextField();
         wordEntered.setLayoutX(17.0);
-        wordEntered.setLayoutY(105.0);
+        wordEntered.setLayoutY(59.0);
         wordEntered.setPrefHeight(27.0);
         wordEntered.setPrefWidth(211.0);
         anchorPane1.getChildren().add(wordEntered);
         Button submitButton = new Button();
         submitButton.setLayoutX(63.0);
-        submitButton.setLayoutY(146.0);
+        submitButton.setLayoutY(101.0);
         submitButton.setMnemonicParsing(false);
         submitButton.setPrefHeight(27.0);
         submitButton.setPrefWidth(120.0);
         submitButton.setText("Submit Word");
+        submitButton.setStyle("-fx-background-color: " + button_color + "; -fx-text-fill: " + text_color + "; " +
+                "-fx-border-color: " + text_color +";");
         anchorPane1.getChildren().add(submitButton);
         Button endButton = new Button();
         endButton.setLayoutX(63.0);
-        endButton.setLayoutY(198.0);
+        endButton.setLayoutY(271.0);
         endButton.setMnemonicParsing(false);
         endButton.setPrefHeight(27.0);
         endButton.setPrefWidth(120.0);
+        endButton.setStyle("-fx-background-color: " + button_color + "; -fx-text-fill: " + text_color + "; " +
+                "-fx-border-color: " + text_color +";");
         endButton.setText("End Game");
+
         anchorPane1.getChildren().add(endButton);
         Button backButton = new Button();
         backButton.setLayoutX(63.0);
-        backButton.setLayoutY(303.0);
+        backButton.setLayoutY(363.0);
         backButton.setMnemonicParsing(false);
         backButton.setPrefHeight(27.0);
         backButton.setPrefWidth(120.0);
         backButton.setText("Back to home");
+        backButton.setStyle("-fx-background-color: " + button_color + "; -fx-text-fill: " + text_color + "; " +
+                "-fx-border-color: " + text_color +";");
         anchorPane1.getChildren().add(backButton);
+        StackPane stackforisvalid = new StackPane();
+        stackforisvalid.setLayoutX(45.0);
+        stackforisvalid.setLayoutY(142.0);
+        stackforisvalid.setPrefHeight(35.0);
+        stackforisvalid.setPrefWidth(155.0);
+        Label isValidWord = new Label();
+//        isValidWord.setLayoutX(108.0);
+//        isValidWord.setLayoutY(156.0);
+        isValidWord.setStyle("-fx-background-color: " + background_color + "; -fx-text-fill: " + text_color + ";");
+        stackforisvalid.getChildren().add(isValidWord);
+        anchorPane1.getChildren().add(stackforisvalid);
+//        anchorPane1.getChildren().add(isValidWord);
+        Button hintButton = new Button();
+        hintButton.setLayoutX(63.0);
+        hintButton.setLayoutY(189.0);
+        hintButton.setMnemonicParsing(false);
+        hintButton.setPrefHeight(27.0);
+        hintButton.setPrefWidth(120.0);
+        hintButton.setText("Get Hint");
+        hintButton.setStyle("-fx-background-color: " + button_color + "; -fx-text-fill: " + text_color + "; " +
+                "-fx-border-color: " + text_color +";");
+        anchorPane1.getChildren().add(hintButton);
+        StackPane stackforhint = new StackPane();
+        stackforhint.setLayoutX(44.0);
+        stackforhint.setLayoutY(226.0);
+        stackforhint.setPrefHeight(17.0);
+        stackforhint.setPrefWidth(30.0);
+        Label hintMessage = new Label();
+//        hintMessage.setLayoutX(109.0);
+//        hintMessage.setLayoutY(237.0);
+        hintMessage.setStyle("-fx-background-color: " + background_color + "; -fx-text-fill: " + text_color + ";");
+        stackforhint.getChildren().add(hintMessage);
+        anchorPane1.getChildren().add(stackforhint);
         BoggleGrid boggleGrid = boggleGame.getGrid();
         String boggleBoard = boggleGrid.toString();
         boggleboardDisplay.setText(boggleBoard);
@@ -88,5 +166,17 @@ public class BoardView {
         Scene scene = new Scene(splitPane0);
         stage.setScene(scene);
         stage.show();
+
+        this.backButton = backButton;
+        this.submitButton = submitButton;
+        this.endButton = endButton;
+        this.wordEntered = wordEntered;
+        this.isValidWord = isValidWord;
+    }
+    public void registerMouseListener(BoardViewController boardController) {
+//        endButton.setOnAction(BoardViewController::showStatsPage);
+        submitButton.setOnAction(BoardViewController::checkWord);
+        backButton.setOnAction(BoardViewController::switchToHomePage);
+
     }
 }
--- code/HomeViewController.java
+++ code/HomeViewController.java
@@ -12,9 +12,21 @@ public class HomeViewController {
         this.boggleGame = boggleGame;
     }
 
+    public BoggleGame updatedBoggleGame() {
+        String mode_chosen = (String) homeView.mode_mapping.get((homeView.mode_menu.getValue()));
+        String theme_chosen = (homeView.theme_menu.getValue()).toUpperCase();
+        int board_size = Integer.parseInt(homeView.board_menu.getValue().substring(0, 1));
+        return new BoggleGame(BoggleGame.GameModes.valueOf(mode_chosen), BoggleGame.Themes.valueOf(theme_chosen), board_size);
+
+    }
+
     public void switchToBoardPage(ActionEvent actionEvent){
         try {
-            BoardView boardView = new BoardView(boggleGame, homeView.stage);
+            boggleGame = updatedBoggleGame();
+            BoardView boardView = new BoardView(homeView, boggleGame);
+            BoardViewController boardViewController = new BoardViewController(boardView);
+            boggleGame.startGame();
+            boardView.registerMouseListener(boardViewController);
         }
         catch (Exception e) {
             e.printStackTrace();
